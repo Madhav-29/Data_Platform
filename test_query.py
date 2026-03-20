99328509fd01abf46a036e0bdc97810ed098f44a
Madhav Sai Avanigadda <amadhavsai29@gmail.com>
2026-03-19 21:49:29 -0400

    Add files via upload

--- test_query.py
+++ test_query.py
@@ -2,6 +2,6 @@ import duckdb
 
 conn = duckdb.connect("warehouse.db")
 
-result = conn.execute("SELECT * FROM raw_products LIMIT 5").fetchdf()
+result = conn.execute("SELECT * FROM dim_products LIMIT 5").fetchdf()
 
 print(result)
